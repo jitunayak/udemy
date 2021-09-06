69997d18f07745f98ca14dd868b4185c89df314e
Jitu Nayak <jitunayak715@gmail.com>
2021-09-07 01:18:23 +0530

    pull to refresh added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,49 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  ActivityIndicator,
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
+    RefreshControl,
+    SafeAreaView,
+    ScrollView,
+    Text,
 } from "react-native";
-import { Categories, Course } from "../models";
-import { DataStore, Predicates, SortDirection } from "@aws-amplify/datastore";
+import {Categories} from "../models";
+import {DataStore, Predicates, SortDirection} from "@aws-amplify/datastore";
 import tw from "tailwind-react-native-classnames";
 import CoursesList from "./CoursesList";
 
-function Home({ navigation }) {
-  const [categories, setCategories] = useState<Categories[]>([]);
-  const [coursesList, setCoursesList] = useState<Course[]>([]);
-  const [loaded, setLoaded] = useState(false);
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
-  useEffect(() => {
-    DataStore.query(Categories, Predicates.ALL, {
-      sort: (s) => s.createdAt(SortDirection.DESCENDING),
-    }).then((categories) => {
-      setCategories(categories);
-      setLoaded(true);
-    });
-  }, []);
+function Home({navigation}) {
+    const [categories, setCategories] = useState<Categories[]>([]);
+    const [refreshing, setRefreshing] = React.useState(true);
 
-  return (
-    <SafeAreaView style={tw`m-2`}>
-      {loaded ? <></> : <ActivityIndicator />}
-      <Text style={tw`text-2xl font-bold text-red-600  p-2`}>Udemy Home</Text>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {categories.map((c, index) => {
-          return (
-            <CoursesList
-              navigation={navigation}
-              categoryId={c.id}
-              key={index}
-              categoryName={c.categoryName}
-            />
-          );
-        })}
-        <Text style={tw`p-10 self-center`}>@copyright 2021 Udemy</Text>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    useEffect(() => {
+        DataStore.query(Categories, Predicates.ALL, {
+            sort: (s) => s.createdAt(SortDirection.DESCENDING),
+        }).then((categories) => {
+            setCategories(categories);
+            setRefreshing(false)
+        });
+    }, [refreshing]);
+
+    return (
+        <SafeAreaView style={tw`m-2`}>
+            <Text style={tw`text-2xl font-bold text-red-600  p-2`}>Udemy Home</Text>
+            <ScrollView showsVerticalScrollIndicator={false}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={() => setRefreshing(true)}
+                            />}>
+                {categories.map((c, index) => {
+                    return (
+                        <CoursesList
+                            navigation={navigation}
+                            categoryId={c.id}
+                            key={index}
+                            categoryName={c.categoryName}
+                        />
+                    );
+                })}
+                <Text style={tw`p-10 self-center`}>@copyright 2021 Udemy</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 export default Home;
